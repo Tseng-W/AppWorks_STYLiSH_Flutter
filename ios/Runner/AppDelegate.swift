3e683aed2577f6b0f95be83152a46b56e7443867
Wen <twayne0618@gmail.com>
2023-04-21 13:50:58 +0800

    Week5-PlatformChannel
    1. Add iOS tap pay framework and environment initialization.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,18 @@
-import UIKit
 import Flutter
+import TPDirect
+import UIKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+
+        let appID: Int32 = 123456
+        TPDSetup.setWithAppId(appID, withAppKey: "APP_KEY", with: TPDServerType.sandBox)
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
